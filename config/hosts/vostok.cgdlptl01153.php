d3b474e45732d38583ede9949b3399d214a95a73
Florent <CHANAVAT@CGDLPTL01153.cegidgroup.local>
2016-04-23 11:37:14 +0200

    Config pour test Vostok sur cgdlptl01153

---
+++ config/hosts/vostok.cgdlptl01153.php
@@ -0,0 +1,24 @@
+<?php
+define ('DB_NAME', 'usocrate_vostok');
+define ('DB_USER', 'florent');
+define ('DB_PASSWORD', 'testB');
+define ('DB_HOST', 'localhost');
+define ('APPLI_NAME', 'Vostok Test');
+define ('APPLI_DESCRIPTION', APPLI_NAME);
+define ('APPLI_PUBLISHER', 'Florent Chanavat');
+define ('APPLI_CREATOR', 'Florent Chanavat');
+define ('APPLI_DIR', 'C:\Users\CHANAVAT\Sandbox\usocrate\vostok');
+define ('CLASS_DIR', 'C:\Users\CHANAVAT\Sandbox\usocrate\vostok\classes');
+define ('DATA_DIR', 'C:\Users\CHANAVAT\Sandbox\usocrate\vostok\data');
+define ('APPLI_URL','http://vostok.cgdlptl01153/');
+define ('SKIN_URL', '/skin/');
+define ('IMAGES_URL', SKIN_URL.'images/');
+
+// Yahoo! User Interface
+define ('YUI_SEEDFILE_URI', '/outsourcing/yui_3.17.2/yui/yui-min.js');
+
+// Pure
+define ('PURE_SEEDFILE_URI', '/outsourcing/pure/0.3.0/pure-min.css');
+
+// Bluga.net WebThumb snapping the web
+define('BLUGA_DIR', 'C:\Users\CHANAVAT\Sandbox\usocrate\outsourcing\Bluga.net-Webthumb-API-for-PHP');
