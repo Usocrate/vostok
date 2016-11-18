c8cc219e91370d7e40cbd27443f85c34afcacb73
Florent Chanavat <florent@usocrate.fr>
2016-11-18 10:33:18 +0000

    Réintégration de la clé Google Maps

--- config/boot.php
+++ config/boot.php
@@ -12,8 +12,8 @@ define ( 'BOOTSTRAP_JS_URI', $system->getSkinUrl().'bootstrap/js/bootstrap.min.j
 define ( 'JQUERY_URI', 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js' );
 define ( 'JQUERY_UI_URI', 'https://code.jquery.com/ui/1.11.4/jquery-ui.min.js' );
 
-// Yahoo! User Interface
-define ('YUI_SEEDFILE_URI', '/outsourcing/yui3/build/yui/yui-min.js');
+// Google Maps
+define ('GOOGLE_MAPS_API_KEY', 'ABQIAAAA5b4RSJ4b0l6G30KHth1RyxT9EP-X3iUMqrKgizFQ5dGr1jh7WxSPXnmx1_xPAmOZ-ORb0NFuZDR3SQ');
 
 // Bluga.net WebThumb snapping the web
 define('BLUGA_DIR', '/home/ubuntu/workspace/outsourcing/Bluga.net-Webthumb-API-for-PHP');
