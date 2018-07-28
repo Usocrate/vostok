2df83bce6ff9094e145c6b88923d96715649eb95
Florent Chanavat <florent@usocrate.fr>
2018-07-28 18:46:00 +0000

    Affichage des gens épinglés

--- individual.php
+++ individual.php
@@ -59,7 +59,7 @@ if (!empty($_SESSION['preferences']['individual']['focus'])) {
 <?php include 'navbar.inc.php'; ?>
 <div class="container-fluid">
 	<h1>
-	<?php echo ToolBox::toHtml($doc_title); ?> <small><a href="individual_edit.php?individual_id=<?php echo $individual->getId() ?>"><span class="glyphicon glyphicon-edit"></span></a> <a href="index.php?individual_task_id=pin&individual_id=<?php echo $individual->getId() ?>"><span class="glyphicon glyphicon-pushpin"></span></a></small></h1>
+	<?php echo ToolBox::toHtml($doc_title); ?> <small><a href="individual_edit.php?individual_id=<?php echo $individual->getId() ?>"><span class="glyphicon glyphicon-edit"></span></a> <a href="index.php?individual_task_id=pin&individual_id=<?php echo $individual->getId() ?>&memberships_focus=onLastPinned"><span class="glyphicon glyphicon-pushpin"></span></a></small></h1>
 	<section>
 	<div>	
 		<?php
