a54e1986138633d775099ae749188190204ff029
florent <florent@traktor>
2022-07-20 12:01:03 +0200

    Lien vers imagesloaded corrigé

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ $doc_title = 'Accueil';
 	<script src="<?php echo JQUERY_URI; ?>"></script>
 	<script src="<?php echo JQUERY_UI_URI; ?>"></script>
 	<script src="js/masonry.pkgd.min.js"></script>
-	<script src="<?php echo IMAGESLOADED_URI ?>"></script>
+	<script src="js/imagesloaded.pkgd.min.js"></script>
 	<script src="<?php echo BOOTSTRAP_JS_URI ?>" integrity="<?php echo BOOTSTRAP_JS_URI_INTEGRITY ?>" crossorigin="anonymous"></script>
 </head>
 <body id="indexDoc">
--- societies.php
+++ societies.php
@@ -92,7 +92,7 @@ $doc_title = 'Les sociétés qui m\'intéressent';
 	<script src="<?php echo JQUERY_URI; ?>"></script>
 	<script src="<?php echo JQUERY_UI_URI; ?>"></script>
 	<script src="js/masonry.pkgd.min.js"></script>
-	<script src="<?php echo IMAGESLOADED_URI ?>"></script>
+	<script src="js/imagesloaded.pkgd.min.js"></script>
 	<script src="<?php echo BOOTSTRAP_JS_URI ?>" integrity="<?php echo BOOTSTRAP_JS_URI_INTEGRITY ?>" crossorigin="anonymous"></script>
 </head>
 <body id="societiesListDoc">
