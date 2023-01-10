4fdf1ffaa626bd8f09145ca20d747a6ba155e9b9
florent <florent@traktor>
2023-01-10 10:19:17 +0100

    Mise au propre code javascript

--- membership_edit.php
+++ membership_edit.php
@@ -200,91 +200,87 @@ if ($membership->isSocietyIdentified () && $membership->isIndividualIdentified (
 		<h1 class="bd-title"><?php echo $h1_content ?></h1>
 	<?php echo $fb->toHtml() ?>
 	<section>
-			<form id="membership_form"
-				action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+			<form id="membership_form" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
     		<?php
-						if ($membership->hasId ()) {
-							echo '<input name="membership_id" type="hidden" value="' . $membership->getId () . '" />';
-						}
-						if (isset ( $applicant )) {
-							switch (get_class ( $applicant )) {
-								case 'Society' :
-									echo '<input id="society_id_i" name="society_id" type="hidden" value="' . $applicant->getId () . '" />';
-									break;
-								case 'Individual' :
-									echo '<input id="individual_id_i" name="individual_id" type="hidden" value="' . $applicant->getId () . '" />';
-									break;
-							}
-						}
+				if ($membership->hasId ()) {
+					echo '<input name="membership_id" type="hidden" value="' . $membership->getId () . '" />';
+				}
+				if (isset ( $applicant )) {
+					switch (get_class ( $applicant )) {
+						case 'Society' :
+							echo '<input id="society_id_i" name="society_id" type="hidden" value="' . $applicant->getId () . '" />';
+							break;
+						case 'Individual' :
+							echo '<input id="individual_id_i" name="individual_id" type="hidden" value="' . $applicant->getId () . '" />';
+							break;
+					}
+				}
 
-						if (! $membership->isSocietyIdentified ()) {
+				if (! $membership->isSocietyIdentified ()) {
+					echo '<div class="form-group">';
+					echo '<label for="s_name_i">Nom de la Société</label>';
+					echo '<input id="s_name_i" name="society_name" type="text" class="form-control" maxlength="255" size="35"';
+					echo '/>';
+					echo '</div>';
+				} else {
+					if ($membership->isIndividualIdentified ()) {
+						$relatedSocieties = $membership->getSociety ()->getRelatedSocieties ();
+						if (count ( $relatedSocieties ) > 0) {
 							echo '<div class="form-group">';
-							echo '<label for="s_name_i">Nom de la Société</label>';
-							echo '<input id="s_name_i" name="society_name" type="text" class="form-control" maxlength="255" size="35"';
-							echo '/>';
+							echo '<label for="newsociety_id">Transférer dans une société liée</label>';
+							echo '<select name="newsociety_id" class="form-control">';
+							echo '<option value="">-- choisir --</option>';
+							echo $membership->getSociety ()->getRelatedSocietiesOptionsTags ();
+							echo '</select>';
 							echo '</div>';
-						} else {
-							if ($membership->isIndividualIdentified ()) {
-								$relatedSocieties = $membership->getSociety ()->getRelatedSocieties ();
-								if (count ( $relatedSocieties ) > 0) {
-									echo '<div class="form-group">';
-									echo '<label for="newsociety_id">Transférer dans une société liée</label>';
-									echo '<select name="newsociety_id" class="form-control">';
-									echo '<option value="">-- choisir --</option>';
-									echo $membership->getSociety ()->getRelatedSocietiesOptionsTags ();
-									echo '</select>';
-									echo '</div>';
-								}
-							}
 						}
+					}
+				}
 
-						if (! $membership->isIndividualIdentified ()) {
-							echo '<div id="individual-form-row" class="form-row">';
-							echo '<div class="form-group col-md-4">';
-							echo '<label for="individual_firstName_i">Prénom</label>';
-							echo '<input id="individual_firstName_i" name="individual_firstName" type="text" maxlength="255" class="form-control" />';
-							echo '</div>';
-							echo '<div class="form-group col-md-6">';
-							echo '<label for="individual_lastName_i">Nom</label>';
-							echo '<input id="individual_lastName_i" name="individual_lastName" type="text" maxlength="255" class="form-control" />';
-							echo '</div>';
-							echo '</div>';
-						}
-						?>
+				if (! $membership->isIndividualIdentified ()) {
+					echo '<div id="individual-form-row" class="form-row">';
+					echo '<div class="form-group col-md-4">';
+					echo '<label for="individual_firstName_i">Prénom</label>';
+					echo '<input id="individual_firstName_i" name="individual_firstName" type="text" maxlength="255" class="form-control" />';
+					echo '</div>';
+					echo '<div class="form-group col-md-6">';
+					echo '<label for="individual_lastName_i">Nom</label>';
+					echo '<input id="individual_lastName_i" name="individual_lastName" type="text" maxlength="255" class="form-control" />';
+					echo '</div>';
+					echo '</div>';
+				}
+			?>
 
 			<div class="form-row">
-					<div class="form-group col-md-4 col-sm-12">
-						<label for="title_i">Fonction</label> <input id="title_i"
-							name="title" type="text" list="title_list"
-							value="<?php echo $membership->getTitle(); ?>" size="35"
-							maxlength="255" class="form-control" />
-						<datalist id="title_list">
+				<div class="form-group col-md-4 col-sm-12">
+					<label for="title_i">Fonction</label> <input id="title_i" name="title" type="text" list="title_list" value="<?php echo $membership->getTitle(); ?>" size="35" maxlength="255" class="form-control" />
+					<datalist id="title_list">
 					<?php
 					foreach ( $system->getMembershipTitles () as $t ) {
 						echo '<option>' . ToolBox::toHtml ( $t ['label'] ) . '</option>';
 					}
 					?>
 					</datalist>
-					</div>
-					<div class="form-group col-md-4 col-sm-12">
-						<label for="department_i">Service</label> <input id="department_i"
-							name="department" type="text"
-							value="<?php echo $membership->getDepartment(); ?>" size="35"
-							maxlength="255" class="form-control" />
-					</div>
-					<div class="form-group col-md-2 col-sm-6">
-						<label for="init_year_i">Année d'ouverture</label> <input
-							id="init_year_i" name="init_year" type="text"
-							value="<?php echo $membership->getInitYear(); ?>" size="20"
-							class="form-control" />
-					</div>
-					<div class="form-group col-md-2 col-sm-6">
-						<label for="end_year_i">Année de clôture</label> <input
-							id="end_year_i" name="end_year" type="text"
-							value="<?php echo $membership->getEndYear(); ?>" size="20"
-							class="form-control" />
-					</div>
 				</div>
+				<div class="form-group col-md-4 col-sm-12">
+					<label for="department_i">Service</label> <input id="department_i"
+						name="department" type="text"
+						value="<?php echo $membership->getDepartment(); ?>" size="35"
+						maxlength="255" class="form-control" />
+				</div>
+				<div class="form-group col-md-2 col-sm-6">
+					<label for="init_year_i">Année d'ouverture</label> <input
+						id="init_year_i" name="init_year" type="text"
+						value="<?php echo $membership->getInitYear(); ?>" size="20"
+						class="form-control" />
+				</div>
+				<div class="form-group col-md-2 col-sm-6">
+					<label for="end_year_i">Année de clôture</label> <input
+						id="end_year_i" name="end_year" type="text"
+						value="<?php echo $membership->getEndYear(); ?>" size="20"
+						class="form-control" />
+				</div>
+			</div>
 			
 			<?php if ( empty($_REQUEST['serie']) || strcmp($_REQUEST['serie'], 'similarMembershipInSociety')!=0 || ($lastSavedMembership->hasPhone() || $lastSavedMembership->hasEmail()) ) : ?>
     		<div class="form-row">
@@ -318,44 +314,44 @@ if ($membership->isSocietyIdentified () && $membership->isIndividualIdentified (
 					<a id="membership_url_link" href="#" style="display: none">[voir]</a>
 				</div>
 
-			<?php
-			$serie_options = array ();
-
-			if (isset ( $applicant )) {
-
-				$serie_options ['none'] = 'Ne pas enchaîner';
-
-				switch (get_class ( $applicant )) {
-					case 'Society' :
-						$serie_options ['similarMembershipInSociety'] = 'Enchaîner avec une participation similaire';
-						$serie_options ['anotherMembershipInSociety'] = 'Enchaîner avec une autre participation';
-						break;
-					case 'Individual' :
-						$serie_options ['anotherIndividualMembership'] = 'Enchaîner avec ' . $membership->getHtmlLinkToIndividual ( 'friendly' ) . ' dans une autre société';
-						$serie_options ['anotherIndividualMembershipInSociety'] = 'Enchaîner avec une autre participation';
-						break;
-				}
-			}
-
-			$toCheck = empty ( $_REQUEST ['serie'] ) ? 'none' : $_REQUEST ['serie'];
-
-			if (count ( $serie_options ) > 1) {
-				echo '<div class="form-group">';
-				$i = 0;
-				foreach ( $serie_options as $v => $l ) {
-					$i ++;
-					echo '<div class="form-check form-check-inline">';
-					echo '<input class="form-check-input" type="radio" name="serie" id="serie_opt' . $i . '" value="' . $v . '"';
-					if (strcmp ( $toCheck, $v ) == 0) {
-						echo ' checked';
+				<?php
+				$serie_options = array ();
+	
+				if (isset ( $applicant )) {
+	
+					$serie_options ['none'] = 'Ne pas enchaîner';
+	
+					switch (get_class ( $applicant )) {
+						case 'Society' :
+							$serie_options ['similarMembershipInSociety'] = 'Enchaîner avec une participation similaire';
+							$serie_options ['anotherMembershipInSociety'] = 'Enchaîner avec une autre participation';
+							break;
+						case 'Individual' :
+							$serie_options ['anotherIndividualMembership'] = 'Enchaîner avec ' . $membership->getHtmlLinkToIndividual ( 'friendly' ) . ' dans une autre société';
+							$serie_options ['anotherIndividualMembershipInSociety'] = 'Enchaîner avec une autre participation';
+							break;
 					}
-					echo '>';
-					echo '<label class="form-check-label" for="serie_opt' . $i . '">' . $l . '</label>';
+				}
+	
+				$toCheck = empty ( $_REQUEST ['serie'] ) ? 'none' : $_REQUEST ['serie'];
+	
+				if (count ( $serie_options ) > 1) {
+					echo '<div class="form-group">';
+					$i = 0;
+					foreach ( $serie_options as $v => $l ) {
+						$i ++;
+						echo '<div class="form-check form-check-inline">';
+						echo '<input class="form-check-input" type="radio" name="serie" id="serie_opt' . $i . '" value="' . $v . '"';
+						if (strcmp ( $toCheck, $v ) == 0) {
+							echo ' checked';
+						}
+						echo '>';
+						echo '<label class="form-check-label" for="serie_opt' . $i . '">' . $l . '</label>';
+						echo '</div>';
+					}
 					echo '</div>';
 				}
-				echo '</div>';
-			}
-			?>
+				?>
 			<div>
 				<?php
 				if (isset ( $applicant )) {
@@ -369,11 +365,9 @@ if ($membership->isSocietyIdentified () && $membership->isIndividualIdentified (
 					}
 				}
 				?>
-				<button name="task" type="submit" value="membership_submission"
-						class="btn btn-primary">enregistrer</button>
-				</div>
-
-			</form>
+				<button name="task" type="submit" value="membership_submission"	class="btn btn-primary">enregistrer</button>
+			</div>
+		</form>
 		<?php
 		if ($membership->hasId ()) {
 			echo '<p>Tu veux oublier cette participation ? C\'est <a id="delete_a" href="#">ici</a>.</p>';
@@ -388,7 +382,7 @@ if ($membership->isSocietyIdentified () && $membership->isIndividualIdentified (
 	}
 	?>
 </div>
-	<script>
+<script>
 	$(document).ready(function(){
 		$('#s_name_i').autocomplete({
 			minLength: 2,
@@ -413,66 +407,63 @@ if ($membership->isSocietyIdentified () && $membership->isIndividualIdentified (
 </script>
 
 <script type="text/javascript">
-	function displayAlert(id, value) {
-		const i = document.getElementById(id);
-		const aid = id+'_a';
-		const a = document.getElementById(aid);
-		
-		if (value !== null && value !== undefined && value.length>0) {
-	        if (a!==null) {
-	        	a.remove();
-	        }
-	        var html = '<div id="'+aid+'" class="alert alert-info">'+value+'</div>';
-	        i.insertAdjacentHTML('afterend', html);
-		} else {
-	        if (a!==null) {
-	        	a.remove();
-	        }
-		}
-	}
-
-	function removeDisplayedAlert() {
-
-		const alerts = document.querySelectorAll('.alert');
-		
-		if (alerts !== null) {
-			for (const a of alerts) {
-				a.remove();
-			}
-		}
-	}
-	
 	document.addEventListener("DOMContentLoaded", function() {
 
-		<?php if(!$membership->hasId()): ?>
-		function checkIndividualMemberships(e) {
+		const ifni = document.getElementById('individual_firstName_i');
+		const ilni = document.getElementById('individual_lastName_i');
 
-			const ifni = document.getElementById('individual_firstName_i');
-			const ilni = document.getElementById('individual_lastName_i');
+		function displayAlert(id, value) {
+			let i = document.getElementById(id);
+			let aid = id+'_a';
+			let a = document.getElementById(aid);
+			
+			if (value !== null && value !== undefined && value.length>0) {
+		        if (a!==null) {
+		        	a.remove();
+		        }
+		        let html = '<div id="'+aid+'" class="alert alert-info">'+value+'</div>';
+		        i.insertAdjacentHTML('afterend', html);
+			} else {
+		        if (a!==null) {
+		        	a.remove();
+		        }
+			}
+		}
+
+		function removeDisplayedAlert() {
+			let alerts = document.querySelectorAll('.alert');
+			
+			if (alerts !== null) {
+				for (let a of alerts) {
+					a.remove();
+				}
+			}
+		}
+
+		function checkIndividualMemberships(e) {
 
 			removeDisplayedAlert();
 
-			var exe_condition = ifni.value.length>0 && ilni.value.length>0;
+			let exe_condition = ifni.value.length>0 && ilni.value.length>0;
 			if (exe_condition == false) {
-				console.log('pas glop ! : '+ ifni.value + ' et '+ilni.value);
 				return false;
 			}  
 
 			event.preventDefault();
 			
-			var xhr = new XMLHttpRequest();
+			let xhr = new XMLHttpRequest();
 			xhr.open("GET", "api/memberships/?individual_firstName="+ifni.value+"&individual_lastName="+ilni.value+"&society_id="+document.getElementById('society_id_i').value, true);
 			xhr.responseType = 'json';
 			
 			xhr.onreadystatechange = function () {
 				if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
 				
-					var titles=[];
-					for (const m of this.response) {
+					let titles=[];
+					for (let m of this.response) {
 						titles.push(m.title);
 					}
 					if (titles.length>0) {
-						var html = '';
+						let html = '';
 						html+= ifni.value+" "+ilni.value+" est déjà ";
 						for (i=0;i<titles.length;i++) {
 							html+= titles[i];
@@ -483,29 +474,26 @@ if ($membership->isSocietyIdentified () && $membership->isIndividualIdentified (
 							}
 						}
 						html+='.<br>';
-
-						//console.log(html);
-						
 						displayAlert('individual-form-row', html);
 					}
 		    	}				  
 			  };
 			  xhr.send();
 		}
-		if (document.getElementById('individual_firstName_i') !==null) {
-			document.getElementById('individual_firstName_i').addEventListener('change', checkIndividualMemberships);
-			document.getElementById('individual_firstName_i').focus();
+
+		if (ifni !==null) {
+			ifni.addEventListener('change', checkIndividualMemberships);
+			ifni.focus();
 		}
 		
-		if (document.getElementById('individual_lastName_i') !==null) {
-			document.getElementById('individual_lastName_i').addEventListener('change', checkIndividualMemberships);
+		if (ilni !==null) {
+			ilni.addEventListener('change', checkIndividualMemberships);
 		}
-		<?php endif; ?>
 		
 		<?php if($membership->hasId()): ?>
 		document.getElementById('delete_a').addEventListener('click', function (event) {
 			event.preventDefault();
-			var xhr = new XMLHttpRequest();
+			let xhr = new XMLHttpRequest();
 			xhr.open("POST", "api/memberships/", true);
 			xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 			xhr.responseType = 'json';
